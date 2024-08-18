5767fc31b5b34b7b3f1c8c1ace75fdb61cad225d
SumedhVats <sumedhvats2004@gmail.com>
2024-08-18 12:51:09 +0530

    first commit

---
+++ Sumedh/src/main/java/com/Blacksheep/Sumedh/SumedhApplication.java
@@ -0,0 +1,13 @@
+package com.Blacksheep.Sumedh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumedhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumedhApplication.class, args);
+	}
+
+}
---
+++ Sumedh/src/test/java/com/Blacksheep/Sumedh/SumedhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blacksheep.Sumedh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumedhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
